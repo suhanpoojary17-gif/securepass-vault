c6e5ae74b858a22004b3899b376d0b89dca8c786
Suhan Poojary <suhanpoojary17@gmail.com>
2026-06-04 16:16:41 +0530

    Front end setup. Backend strict cors addition

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ const passwordRoutes = require("./routes/passwordRoutes");
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
 app.use(express.json());
 
 // MongoDB Connection
